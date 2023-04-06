402c6ffd23ce1b37c4f9c09fb300265e839d1887
j3rryl <jeremymunroe0@gmail.com>
2023-04-06 06:01:16 +0300

    first commit

--- app/src/main/java/com/example/broadcastreceivers/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceivers/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.broadcastreceivers
 
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val receiver=AirplaneModeChangedReceiver()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var intent = Intent(this,CustomReceiver::class.java)
         intent.action="com.example.CUSTOM_INTENT"
         sendBroadcast(intent)
+
+        var receiverIntent = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(receiver,receiverIntent)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val receiver=AirplaneModeChangedReceiver()
+        unregisterReceiver(receiver)
     }
 }
